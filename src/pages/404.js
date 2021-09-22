e7a9bbb126074ab7225a1bb3c712a45162aa39ea
Emmanuel Gautier <git@emmanuelgautier.fr>
2021-09-22 23:23:49 +0200

    fix: use gatsby seo component for 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
+import { GatsbySeo } from 'gatsby-plugin-next-seo'
 import React from 'react'
 
 import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
+    <GatsbySeo title="404: Not found" />
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
